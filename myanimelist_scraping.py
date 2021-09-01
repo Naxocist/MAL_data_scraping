a974cdcfafff97f73e0363e8ea3c66de3dba8718
Naxocist <subforwork@gmail.com>
2021-09-01 13:37:58 +0700

    Improve performance
    Adding semaphore function

--- myanimelist_scraping.py
+++ myanimelist_scraping.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 from gevent import monkey as curious_george
 curious_george.patch_all(thread=False, select=False)
 import requests
-import grequests
 import csv
 import math
 import aiohttp
 import asyncio
+import time
 
 animes_name = []
 animes_episode = []
@@ -16,7 +16,6 @@ animes_pic = []
 animes_season = []
 animes_genre = []
 urls = []
-end = 0
 
 catogories = requests.get('https://myanimelist.net/anime.php').text
 soup_g = BeautifulSoup(catogories, 'lxml')
@@ -37,6 +36,8 @@ for k, v in pair.items():
         url = "https://myanimelist.net" + k + f"?page={i}"
         urls.append(url)
 
+print(urls)
+
 
 async def name_link(session, url):
     async with session.get(url) as resp:
@@ -46,26 +47,27 @@ async def name_link(session, url):
         for index in data:
             name = index.text
             link = index['href']
+            if name in animes_name:
+                continue
             animes_name.append(name)
             animes_link.append(link)
-            print(name, link)
             print(url)
 
 
-async def other_info(session, url):
+async def other_info(session, url):  # fetch other information
     async with session.get(url) as resp:
+        print("Scraped:", url)
         response = await resp.text()
         s = BeautifulSoup(response, 'html.parser')
-        p = s.find('img', itemprop="image")['data-src']
+        p = s.find('img', itemprop="image")
         e = s.find('span', id="curEps")
         s = s.find('span', class_="information season")
-        r = s.find('span', class_="numbers ranked")
+        p = "" if p is None else p['data-src']
+        e = "" if e is None else e.text
+        s = "" if s is None else s.text
         animes_pic.append(p)
         animes_episode.append(e)
         animes_season.append(s)
-        animes_rank.append(r)
-        print(s, p, e, s, r)
-        print(url)
 
 
 async def semaphore(session, url, sem):
@@ -73,26 +75,43 @@ async def semaphore(session, url, sem):
         return await name_link(session, url)
 
 
-async def main(links: list):
-    sem = asyncio.Semaphore(7)  # task grouping
+async def alt_semaphore(session, url, sem):
+    async with sem:
+        return await other_info(session, url)
+
+
+async def main(u: list):  # main async function
+    sem = asyncio.Semaphore(50)  # task grouping
     async with aiohttp.ClientSession() as session:
-        tasks = [asyncio.create_task(semaphore(session, url, sem)) for url in links]
-        gather = await asyncio.gather(*tasks)
+        tasks = [asyncio.create_task(semaphore(session, url, sem)) for url in u]
+        await asyncio.gather(*tasks)
 
 
-n = asyncio.get_event_loop()
-n.run_until_complete(main(urls))
-n.close()
+async def fetch_other(u: list):
+    sem = asyncio.Semaphore(50)
+    async with aiohttp.ClientSession() as session:
+        tasks = [asyncio.create_task(alt_semaphore(session, url, sem)) for url in u]
+        await asyncio.gather(*tasks)
 
-print("Finished Scraping")
+
+# urls = ["https://myanimelist.net/anime/genre/1/Action"]
+
+asyncio.get_event_loop().run_until_complete(main(urls))
+
+with open('animes_link.txt', mode='w', encoding='utf-8') as l:
+    l.write("Backup animes_list\n")
+    l.write("l = [")
+    for i in animes_link:
+        l.write(f"\'{i}\',")
+    l.write("]")
+
+print("Done scraping + Add to txt file")
 print("There are", len(animes_name), "animes")
 
-p = asyncio.get_event_loop()
-p.run_until_complete(main(animes_link))
-p.close()
+asyncio.get_event_loop().run_until_complete(fetch_other(animes_link))
 
-print("name", animes_name)
-print("link", animes_link)
+print("name: ", animes_name)
+print("link: ", animes_link)
 print("pic: ", animes_pic)
 print("season: ", animes_season)
 print("ep: ", animes_episode)
@@ -100,7 +119,7 @@ print("rank: ", animes_rank)
 
 with open('animes.csv', 'w', encoding="utf8", newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['name', 'episode', 'rank', 'link', 'pic', 'season', 'genre'])
+    writer.writerow(['name', 'episode', 'link', 'pic', 'season', 'genre'])
     for i in range(len(animes_name)):
-        writer.writerow([animes_name[i], animes_episode[i], animes_rank[i], animes_link[i], animes_pic[i],
+        writer.writerow([animes_name[i], animes_episode[i], animes_link[i], animes_pic[i],
                          animes_season[i]])
