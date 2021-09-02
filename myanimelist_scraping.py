401f317bfdb8e8ad4c56c808041da47a735094b8
Naxocist <73634957+Naxocist@users.noreply.github.com>
2021-09-02 22:47:17 +0700

    New algorithm for scrape animes dataset
    faster and more effiecient ways to scrape the data

--- myanimelist_scraping.py
+++ myanimelist_scraping.py
@@ -6,105 +6,90 @@ import aiohttp
 import asyncio
 import time
 
-animes_name = []
-animes_episode = []
-animes_rank = []
-animes_link = []
-animes_pic = []
-animes_season = []
-animes_genre = []
-urls = []
+animes_name, animes_episode, animes_rank, animes_link, animes_pic, animes_season, \
+    animes_season, urls = [[] for _ in range(8)]
 
-catogories = requests.get('https://myanimelist.net/anime.php').text
-soup_g = BeautifulSoup(catogories, 'lxml')
-
+req = requests.get('https://myanimelist.net/anime.php').text
+soup_g = BeautifulSoup(req, 'lxml')
 genre_group = soup_g.find_all('a', class_='genre-name-link')
 genres = [i['href'] for i in genre_group]
 del genres[43:]
 
 quantities = [q.text for q in genre_group]
 del quantities[43:]
+
 for i, j in enumerate(quantities):
     quantities[i] = math.ceil(int(j[j.index("(") + 1:-1].replace(',', '')) / 100)
 
-pair = {d: quantities[i] for i, d in enumerate(genres)}
+# -------------------------------------------------------------------------------------
+
+pair = {"https://myanimelist.net" + d: quantities[i] for i, d in enumerate(genres)}
 
 for k, v in pair.items():
-    for i in range(1, v+1):
-        url = "https://myanimelist.net" + k + f"?page={i}"
-        urls.append(url)
+    urls.extend([k + f"?page={i}" for i in range(1, v+1)])
+
+print(urls)
+
+# urls = urls[:1]
 
 
-async def name_link(session, url):
-    async with session.get(url) as resp:
-        response = await resp.text()
-        print("scraping:", url)
-        s = BeautifulSoup(response, 'html.parser')
-        return s
+async def fetch(session, url):
+    async with session.get(url) as request:
+        response = await request.text()
+        print(url)
+        return response
 
 
-async def other_info(session, url):  # fetch other information
-    async with session.get(url) as resp:
-        response = await resp.text()
-        print("scraping anime:", url)
-        s = BeautifulSoup(response, 'html.parser')
-        return s
-
-
-async def semaphore(session, url, sem):
-    async with sem:
-        return await name_link(session, url)
-
-
-async def alt_semaphore(session, url, sem):
-    async with sem:
-        return await other_info(session, url)
-
-
-async def main(u: list):  # main async function
-    sem = asyncio.Semaphore(100)  # task grouping
+async def main(urls):
     async with aiohttp.ClientSession() as session:
-        tasks = [asyncio.create_task(semaphore(session, url, sem)) for url in u]
-        time.sleep(1)
-        return await asyncio.gather(*tasks)
 
+        task = [fetch(session, url) for url in urls]
+        return await asyncio.gather(*task)
 
-async def fetch_other(u: list):
-    sem = asyncio.Semaphore(100)
-    async with aiohttp.ClientSession() as session:
-        tasks = [asyncio.create_task(alt_semaphore(session, url, sem)) for url in u]
-        time.sleep(1)
-        return await asyncio.gather(*tasks)
+# for i in range(len(urls)):
+#
+#     link_temp = []
+#     url = urls[:1]
 
+    # print(url)
+name_link = asyncio.get_event_loop().run_until_complete(main(urls))
 
-# urls = ["https://myanimelist.net/anime/genre/1/Action"]
+for nl in name_link:
+    html = BeautifulSoup(nl, 'html.parser')
+    data = html.find_all('a', class_='link-title')
+    for d in data:
+        name = d.text
+        link = d['href']
 
-result = asyncio.get_event_loop().run_until_complete(main(urls))
-for r in result:
-    data = r.find_all('a', class_='link-title')
-    for index in data:
-        name = index.text
-        link = index['href']
         if name in animes_name:
             continue
         animes_name.append(name)
         animes_link.append(link)
         print(name, link)
 
-print("Done scraping + Add to txt file")
+print(f"Big scrape finished or server restricted")
 print("There are", len(animes_name), "animes")
 
-result = asyncio.get_event_loop().run_until_complete(fetch_other(animes_link))
-for r in result:
-    p = r.find('img', itemprop="image")
-    e = r.find('span', id="curEps")
-    s = r.find('span', class_="information season")
-    p = "" if p is None else p['data-src']
-    e = "" if e is None else e.text
-    s = "" if s is None else s.text
-    animes_pic.append(p)
-    animes_episode.append(e)
-    animes_season.append(s)
+other_info = asyncio.get_event_loop().run_until_complete(main(animes_link))
+
+for index, r in enumerate(other_info):
+    print(index + 1)
+    html = BeautifulSoup(r, 'html.parser')
+    p = html.find('img', itemprop="image")
+    e = html.find('span', id="curEps")
+    s = html.find('span', class_="information season")
+
+    picture = p['data-src'] if p is not None else ""
+    ep = e.text if e is not None else ""
+    season = s.text if s is not None else ""
+
+    animes_pic.append(picture)
+    animes_episode.append(ep)
+    animes_season.append(season)
+    print(picture, ep, season)
+    # del urls[0]
+    # print("Break for 5 minutes")
+    # time.sleep(300)
 
 print("name: ", animes_name)
 print("link: ", animes_link)
